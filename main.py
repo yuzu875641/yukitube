6cc24f29af70e9109d75bd873c6e21bbe2d95847
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-11-15 01:50:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def access_yuki(credentials: HTTPBasicCredentials = Depends(security)): #Basicèª
 
 @app.get("/", response_class=HTMLResponse)
 def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
-    if check_cokie(access_yuki):
+    if check_cokie(Depends(access_yuki)):
         response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
         return template("home.html",{"request": request})
     print(check_cokie(yuki))
