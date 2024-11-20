d98d9b4d36cfcd034f1dcb5477e95adbf3c4bae6
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-11-20 00:12:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def get_data(videoid):
     t = json.loads(apirequest(r"api/v1/videos/"+ urllib.parse.quote(videoid)))
     if not t["type"] == "video":
         raise APItimeoutError("APIエラー")
-    return [{"id":i["videoId"],"title":i["title"],"authorId":i["authorId"],"author":i["author"]} for i in t["recommendedVideos"]],list(reversed([i["url"] for i in t["formatStreams"]]))[:2],t["descriptionHtml"].replace("\n","<br>"),t["title"],t["authorId"],t["author"],t["authorThumbnails"][-1]["url"]
+    return [[{"id":i["videoId"],"title":i["title"],"authorId":i["authorId"],"author":i["author"]} for i in t["recommendedVideos"]],list(reversed([i["url"] for i in t["formatStreams"]]))[:2],t["descriptionHtml"].replace("\n","<br>"),t["title"],t["authorId"],t["author"],t["authorThumbnails"][-1]["url"]]
 
 def get_search(q,page):
     global logs
